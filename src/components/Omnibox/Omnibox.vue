f91626b7ad44132d90bdac5cd0b8438ed92ffe59
Gabryjiel <gabriel.kudyba@gmail.com>
2025-03-05 19:24:03 +0100

    feat: Omnibox can be focused by keys Ctrl+K

--- src/components/Omnibox/Omnibox.vue
+++ src/components/Omnibox/Omnibox.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, provide, reactive, ref, watch } from 'vue';
+import { computed, onMounted, onUnmounted, provide, reactive, ref, useTemplateRef, watch } from 'vue';
 import OmniboxSuggestionGroup from './OmniboxSuggestionGroup.vue';
 import OmniboxSuggestion from './OmniboxSuggestion.vue';
 import { searchAbilities, searchPokemons, type Suggestion } from '@/lib/listHelpers';
@@ -12,6 +12,7 @@ type SuggestionMap = {
 
 const isOmniboxFocused = ref(false);
 const omniboxContent = ref('');
+const omniboxInputRef = useTemplateRef<HTMLInputElement>('omnibox-input')
 const suggestionGroups: SuggestionMap = reactive({
   pokemons: [],
   abilities: [],
@@ -25,6 +26,27 @@ const handleOmniboxChange = (event: Event) => {
   const currentTarget = event.currentTarget as HTMLInputElement;
   omniboxContent.value = currentTarget.value;
 }
+
+const handleKeyboardEvent = (event: KeyboardEvent) => {
+  if (event.key === 'k' && event.ctrlKey) {
+    event.preventDefault()
+    if (document.activeElement === omniboxInputRef.value) {
+      omniboxInputRef.value?.blur()
+      isOmniboxFocused.value = false
+    } else {
+      omniboxInputRef.value?.focus()
+      isOmniboxFocused.value = true
+    }
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('keydown', handleKeyboardEvent);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('keydown', handleKeyboardEvent);
+})
 
 watch(omniboxContent, async (content) => {
   if (content.length === 0) {
@@ -44,12 +66,15 @@ provide('omnibox-content', omniboxContent)
 
 <template>
   <div id="omnibox">
-    <input type="text" placeholder="SEARCH" :value="omniboxContent" @input="handleOmniboxChange"
-      @focus="isOmniboxFocused = true" @blur="isOmniboxFocused = false" />
+    <div class="keys" v-if="!isOmniboxFocused">
+      Ctrl+K
+    </div>
+    <input type="text" placeholder="Search anything" :value="omniboxContent" @input="handleOmniboxChange"
+      @focus="isOmniboxFocused = true" @blur="isOmniboxFocused = false" ref="omnibox-input" />
     <ul id="suggestions" v-if="suggestionCount > 0">
       <OmniboxSuggestionGroup href="/pokemons" text="Pokemons" :isVisible="suggestionGroups.pokemons.length > 0">
         <OmniboxPokemonSuggestion v-for="suggestion in suggestionGroups.pokemons" :key="suggestion.slug"
-          :content="suggestion.name" :href="`/pokemons/${suggestion.id}`" :pokemonId="suggestion.id" />
+          :content="suggestion.name" :href="`/pokemons/${suggestion.id}`" :pokemon-id="suggestion.id" />
       </OmniboxSuggestionGroup>
 
       <OmniboxSuggestionGroup href="/abilities" text="Abilities" :isVisible="suggestionGroups.abilities.length > 0">
@@ -62,32 +87,52 @@ provide('omnibox-content', omniboxContent)
 
 <style scoped>
 #omnibox {
-  width: 640px;
+  width: 400px;
   height: 48px;
   position: relative;
 
   input {
-    text-align: center;
+    text-align: left;
+    text-indent: 1rem;
     width: 100%;
     height: 100%;
-    font-size: x-large;
+    font-size: larger;
     border: 3px solid black;
     border-radius: 10px;
+    background-color: lightsteelblue;
+    color: darkslateblue;
+    font-weight: 100;
 
     &:focus {
-      border-color: red;
+      border-color: darkslateblue;
       outline: none
     }
   }
 }
 
+.keys {
+  position: absolute;
+  right: 5%;
+  top: 29%;
+  padding: 5px 10px;
+  border: 1px solid darkslateblue;
+  color: darkslateblue;
+  border-radius: 10%;
+  display: flex;
+  justify-content: flex;
+  align-items: center;
+  font-size: 11px;
+  user-select: none;
+}
+
 #suggestions {
   position: absolute;
   top: calc(100% + 2px);
-  width: 100%;
+  min-width: 100%;
+  max-width: 200%;
   max-height: 70vh;
-  background-color: lightgray;
-  border: 3px solid black;
+  background-color: lightsteelblue;
+  border: 3px solid darkslateblue;
   border-radius: 10px;
   overflow-y: scroll;
   padding-top: 5px;
