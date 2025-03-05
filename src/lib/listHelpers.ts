0c28523065b11e2e76238292eed4a4bef5b17bc7
Gabryjiel <gabriel.kudyba@gmail.com>
2025-03-05 10:48:24 +0100

    add sorting in omnibox
    sorting is based on search phrase

--- src/lib/listHelpers.ts
+++ src/lib/listHelpers.ts
@@ -28,20 +28,51 @@ function filterName(searchPhrase: string, resource: NamedAPIResource): boolean {
   return resource.name.includes(searchPhrase.toLowerCase());
 }
 
+function sortResources(phrase: string) {
+  return function (a: NamedAPIResource, b: NamedAPIResource): 1 | 0 | -1 {
+    const aLower = -1;
+    const bLower = 1;
+
+    if (a.name === phrase && b.name === phrase) {
+      return 0;
+    } else if (a.name === phrase) {
+      return aLower;
+    } else if (b.name === phrase) {
+      return bLower;
+    } else if (a.name.startsWith(phrase) && b.name.startsWith(phrase)) {
+      const aSlice = a.name.slice(phrase.length);
+      const bSlice = b.name.slice(phrase.length);
+      return aSlice < bSlice ? aLower : aSlice === bSlice ? 0 : bLower;
+    } else if (a.name.startsWith(phrase)) {
+      return aLower;
+    } else if (b.name.startsWith(phrase)) {
+      return bLower;
+    }
+
+    return a.name < b.name ? aLower : a.name === b.name ? 0 : bLower;
+  };
+}
+
 function reduceResource(
   resources: NamedAPIResourceList,
   searchPhrase: string,
   type: SuggestionType,
   nameFormatFn?: (name: string) => string,
+  limit: number = 10,
 ): Suggestion[] {
-  return resources.results.reduce((acc, cur) => {
-    if (!filterName(searchPhrase, cur)) {
-      return acc;
-    }
+  const result = new Array<Suggestion>();
 
-    acc.push(createSugestion(cur, type, nameFormatFn));
-    return acc;
-  }, new Array<Suggestion>());
+  for (const resource of resources.results.toSorted(sortResources(searchPhrase))) {
+    if (filterName(searchPhrase, resource)) {
+      result.push(createSugestion(resource, type, nameFormatFn));
+
+      if (result.length >= limit) {
+        return result;
+      }
+    }
+  }
+
+  return result;
 }
 
 export async function searchPokemons(searchPhrase: string): Promise<Suggestion[]> {
