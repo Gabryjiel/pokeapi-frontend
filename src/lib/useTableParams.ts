5789424468b8a3d9e0c9e74842b6f04bb03962c7
Gabryjiel <gabriel.kudyba@gmail.com>
2025-03-11 10:28:59 +0100

    feat: add hook UseTableParams for pagination and ordering

---
+++ src/lib/useTableParams.ts
@@ -0,0 +1,40 @@
+import { useRouteQuery } from '@vueuse/router';
+
+export type OrderType = '' | 'asc' | 'desc';
+
+export function useTableParams() {
+  const page = useRouteQuery('page', 1, { mode: 'replace', transform: Number });
+  const orderBy = useRouteQuery('orderBy', '' as string, { mode: 'replace' });
+  const orderType = useRouteQuery('orderType', '' as OrderType, { mode: 'replace' });
+
+  const setPage = (newPage: number) => {
+    if (newPage < 1) {
+      return;
+    }
+
+    page.value = newPage;
+  };
+
+  const changeOrder = (newOrderBy: string) => {
+    if (orderBy.value === newOrderBy) {
+      if (orderType.value === '') {
+        orderType.value = 'asc';
+      } else if (orderType.value === 'asc') {
+        orderType.value = 'desc';
+      } else {
+        orderType.value = '';
+      }
+    } else {
+      orderBy.value = newOrderBy;
+      orderType.value = 'asc';
+    }
+  };
+
+  return {
+    page,
+    orderBy,
+    orderType,
+    setPage,
+    changeOrder,
+  };
+}
