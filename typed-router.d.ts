126725878859e3def0175024427bc285e300845c
Gabryjiel <gabriel.kudyba@gmail.com>
2025-04-25 16:39:41 +0200

    feat: add moves.[id], types[id] empty pages

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -27,9 +27,11 @@ declare module 'vue-router/auto-routes' {
     '/builder.teams': RouteRecordInfo<'/builder.teams', '/builder/teams', Record<never, never>, Record<never, never>>,
     '/items': RouteRecordInfo<'/items', '/items', Record<never, never>, Record<never, never>>,
     '/moves': RouteRecordInfo<'/moves', '/moves', Record<never, never>, Record<never, never>>,
+    '/moves.[id]': RouteRecordInfo<'/moves.[id]', '/moves/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
     '/natures': RouteRecordInfo<'/natures', '/natures', Record<never, never>, Record<never, never>>,
     '/pokemons': RouteRecordInfo<'/pokemons', '/pokemons', Record<never, never>, Record<never, never>>,
     '/pokemons.[id]': RouteRecordInfo<'/pokemons.[id]', '/pokemons/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
     '/types': RouteRecordInfo<'/types', '/types', Record<never, never>, Record<never, never>>,
+    '/types.[id]': RouteRecordInfo<'/types.[id]', '/types/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
   }
 }
