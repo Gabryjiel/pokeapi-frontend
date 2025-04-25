1eb3a05c3310551f798959902fab17ee2ccea77f
Gabryjiel <gabriel.kudyba@gmail.com>
2025-04-25 12:33:50 +0200

    fix: rework capitalize, getPokemonDisplayName functions

--- src/lib/stringHelpers.ts
+++ src/lib/stringHelpers.ts
@@ -1,9 +1,29 @@
-export function capitalize<T extends string>(target: T): Capitalize<T> {
+export function capitalize<T extends string>(target?: T): Capitalize<T> {
+  if (target === undefined) {
+    return '' as Capitalize<T>;
+  }
+
   return (target.at(0)?.toUpperCase() + target.slice(1)) as Capitalize<T>;
 }
 
-export function getPokemonDisplayName(pokemonName: string): string {
-  return capitalize(pokemonName).replaceAll('-', ' ');
+export function getPokemonDisplayName(pokemonName?: string): string {
+  if (pokemonName === undefined) {
+    return '';
+  }
+
+  let result = '';
+
+  for (let i = 0; i < pokemonName.length; i++) {
+    if (pokemonName[i] === '-') {
+      result += ' ';
+    } else if (result[i - 1] === ' ' || i === 0) {
+      result += pokemonName[i].toUpperCase();
+    } else {
+      result += pokemonName[i];
+    }
+  }
+
+  return result;
 }
 
 export function getIdFromUrl(url: string): number {
